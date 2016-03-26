c5e8f7a3ea1b2f5b02e39a33420c38a2e290908b
Pierre Leray <pierreleray64@gmail.com>
2016-03-26 20:12:57 +0100

    Fix travis.yml test problem, remove exit(1) for qa test

--- timevortex/management/commands/timevortex.py
+++ timevortex/management/commands/timevortex.py
@@ -26,7 +26,7 @@ ARGUMENTS = {
     },
     OPTION_QA: {
         # KEY_COMMAND: ["pylint *.py */*.py */*/*.py */*/*/*.py -f html --rcfile=.pylintrc > qa.html"],
-        KEY_COMMAND: ["prospector -F"],
+        KEY_COMMAND: ["prospector -F -0"],
         KEY_HELP_TEXT: "Validate qualilty of code using pylint and configuration .pylintrc",
     },
     OPTION_VALIDATE: {
